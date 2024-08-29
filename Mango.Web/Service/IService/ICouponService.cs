5f2ee4a258e2f6b461f31ec9ff0020cb8d3bee77
asileshi <abebesileshi828@gmail.com>
2024-08-29 21:54:25 +0300

    connected the frontend with the api

--- Mango.Web/Service/IService/ICouponService.cs
+++ Mango.Web/Service/IService/ICouponService.cs
@@ -6,6 +6,7 @@ public interface ICouponService
 {
     Task<ResponseDto?> GetCouponAsync(string couponCode);
     Task<ResponseDto?> GetAllCouponAsync();
+    Task<ResponseDto?> GetCouponById(int id);
     Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto);
     Task<ResponseDto?> DeleteCouponAsync(int id);
     Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto);
